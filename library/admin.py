e498b091c679cb6ff1569330c52a6976ec32a7a4
Anton Tsikhanovich <alluspeh@gmail.com>
2026-09-13 12:18:22 +0200

    Add django rest framework

--- library/admin.py
+++ library/admin.py
@@ -1,4 +1,4 @@
-from email.headerregistry import Group
+from django.contrib.auth.models import Group
 
 from django.contrib import admin
 
--- library_service/settings.py
+++ library_service/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "library",
     "user",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
